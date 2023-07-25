18484fc66df96c8677a8e14321bd7f5b1ebd0b55
joceosorio <osoriojoce19@gmail.com>
2023-07-25 04:26:42 -0600

    login
    cambios de longin

--- barberia5/src/app/app.module.ts
+++ barberia5/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { PerfiluserComponent } from './auth/perfiluser/perfiluser.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginnComponent } from './auth/loginn/loginn.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
+    
     RegisterComponent,
     PerfiluserComponent,
-    HomeComponent
+    HomeComponent,
+    LoginnComponent
   ],
   imports: [
     BrowserModule,
