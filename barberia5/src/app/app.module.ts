df86d938be4a5ed35d9efba033611a3a6fd8697a
joceosorio <osoriojoce19@gmail.com>
2023-07-25 04:02:15 -0600

    r
    cambios de registro

--- barberia5/src/app/app.module.ts
+++ barberia5/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { PerfiluserComponent } from './auth/perfiluser/perfiluser.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,7 +9,7 @@ import { HomeComponent } from './components/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
+    
     RegisterComponent,
     PerfiluserComponent,
     HomeComponent
